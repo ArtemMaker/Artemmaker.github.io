d46a0c5ca595bf8c77078728a8ae1f0c607f4262
ArtemMaker <beber4975@gmail.com>
2025-02-17 20:49:32 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1739814308,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/earth-sheet0.png",
+		"images/line-sheet0.png",
+		"images/upgradescreenbutton-sheet0.png",
+		"images/upgradescreenbuttonimg-sheet0.png",
+		"images/camera-sheet0.png",
+		"images/upgearthbutton-sheet0.png",
+		"images/upgearthdesc-sheet0.png",
+		"images/backbutton-sheet0.png",
+		"images/settingsscreenbuttonimg-sheet0.png",
+		"images/savebutton-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
